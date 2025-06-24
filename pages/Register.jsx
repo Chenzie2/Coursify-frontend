c07b42a1518426eec43355e1e1fb3c0e46fd5e4c
jedaqsaul <jedaqsaul@gmail.com>
2025-06-24 23:41:30 +0300

    initialize Register component with form data and error state

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -3,6 +3,17 @@ import { useNavigate } from 'react-router-dom';
 import { UserPlus } from 'lucide-react';
 
 export default function Register() {
+const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [error, setError] = useState('');
+
   return (
     <div>Register</div>
   )
