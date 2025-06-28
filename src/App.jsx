1171b327188e44a61424395b3a36da4a8ccac3d8
g_zawie <gzawie24@gmail.com>
2025-06-28 21:41:23 +0300

    Add files and images

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Navbar from '../components/Navbar';
 import About from '../pages/About'; 
 import LandingPage from '../pages/LandingPage';
 import Login from '../pages/Login';
-import Register from '../pages/Register';
-import MyCourses from '../pages/MyCourses';
+//import Register from '../pages/Register';
+//import MyCourses from '../pages/MyCourses';
 import CourseDetails from '../pages/CourseDetails';
 
 function App() {
-  
+
 
   return (
     //  <ToastContainer position="top-right" autoClose={3000} />
@@ -20,12 +20,12 @@ function App() {
      
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/mycourses" element={<MyCourses />} />
+      {/* <Route path="/register" element={<Register />} />
+      <Route path="/mycourses" element={<MyCourses />} /> */}
       <Route path="/courses/:id" element={<CourseDetails />} />
       
       
-      <Route path="/about" element={<About />} />
+      /* <Route path="/about" element={<About />} />*/
       
     </Routes>
    
