ec765cd6b84b4312809e3a0f2d671a90c46832ba
g_zawie <gzawie24@gmail.com>
2025-06-28 16:13:00 +0300

    Restructure files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+import About from '../pages/About'; 
+import LandingPage from '../pages/LandingPage';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import MyCourses from '../pages/MyCourses';
+import CourseDetails from '../pages/CourseDetails';
+
+function App() {
+  
+
+  return (
+    //  <ToastContainer position="top-right" autoClose={3000} />
+    
+    
+    <Routes>
+     
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/mycourses" element={<MyCourses />} />
+      <Route path="/courses/:id" element={<CourseDetails />} />
+      
+      
+      <Route path="/about" element={<About />} />
+      
+    </Routes>
+   
+  );
+}
+
+export default App;
