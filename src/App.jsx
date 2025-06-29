f3bff7459215ef60daa9f07c24bd58c3fb42e44c
g_zawie <gzawie24@gmail.com>
2025-06-29 13:15:49 +0300

    Fix footer layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState} from 'react';
-import { Routes, Route, useLocation} from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import About from '../pages/About';
 import LandingPage from '../pages/LandingPage';
 import Login from '../pages/Login';
@@ -8,28 +9,25 @@ import Register from '../pages/Register';
 import CourseDetails from '../pages/CourseDetails';
 
 function App() {
-   const location = useLocation();
-  const hideNavbar = ['/login', '/register'].includes(location.pathname);
-
+  const location = useLocation();
+  const hideLayout = ['/login', '/register'].includes(location.pathname);
 
   return (
-    //  <ToastContainer position="top-right" autoClose={3000} />
+    <div className="flex flex-col min-h-screen">
+      {!hideLayout && <Navbar />}
 
-    <>
-    {!hideNavbar && <Navbar />}
-    
-    
-    <Routes>
-     
-      <Route path="/" element={<LandingPage />} /> 
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      {/* <Route path="/mycourses" element={<MyCourses />} /> */}
-      <Route path="/courses/:id" element={<CourseDetails />} />
-      /* <Route path="/about" element={<About />} />*/
-      
-    </Routes>
-   </>
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/courses/:id" element={<CourseDetails />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+
+      {!hideLayout && <Footer />}
+    </div>
   );
 }
 
